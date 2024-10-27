e8b52364e497a98e1a06905dc8b33339c33e0a77
Radin6 <30755101+Radin6@users.noreply.github.com>
2024-10-27 10:08:51 +0100

    config backend prod/dev env var

--- backend/src/config.js
+++ backend/src/config.js
@@ -1,7 +1,7 @@
 import 'dotenv/config';
 
-export const DB_HOST = process.env.DB_HOST || "localhost";
-export const DB_USER = process.env.DB_USER || "nicor";
-export const DB_PASSWORD = process.env.DB_PASSWORD || "";
-export const DB_PORT = process.env.DB_PORT || 3306;
-export const DB_DATABASE = process.env.DB_DATABASE || "twotter_test"
+export const DB_HOST = (process.env.NODE_ENV === "dev") ? process.env.DB_HOST_DEV : process.env.DB_HOST_PROD;
+export const DB_USER = (process.env.NODE_ENV === "dev") ? process.env.DB_USER_DEV : process.env.DB_USER_PROD;
+export const DB_PASSWORD = (process.env.NODE_ENV === "dev") ? process.env.DB_PASSWORD_DEV : process.env.DB_PASSWORD_PROD;
+export const DB_PORT = (process.env.NODE_ENV === "dev") ? process.env.DB_PORT_DEV : process.env.DB_PORT_PROD;
+export const DB_DATABASE = (process.env.NODE_ENV === "dev") ? process.env.DB_DATABASE_DEV : process.env.DB_DATABASE_PROD;
