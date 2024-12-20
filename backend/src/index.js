2c4cd51c3ff721f0b67f305cb17235ddc3e2257d
Radin6 <30755101+Radin6@users.noreply.github.com>
2024-12-20 15:33:02 +0100

    FIX vercel deploy 6

--- backend/src/index.js
+++
@@ -1,9 +0,0 @@
-import app from "../api/index.js"
-import {initializeDB} from "./db.js"
-
-const port = 3000;
-
-initializeDB();
-app.listen(port, () => {
-  console.log(`Server running on port ${port}`)
-})
